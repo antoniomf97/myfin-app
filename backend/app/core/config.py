f8f790eb78431392243078b039197627e9aeab24
antoniomf97 <antonio.mf97@gmail.com>
2026-05-11 22:06:23 +0100

    Added core configs and transactions

---
+++ backend/app/core/config.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Settings:
+    PROJECT_NAME: str = "MyFin App"
+    VERSION: str = "1.0.0"
+    API_PREFIX: str = "/api/v1"  # You can use this later for versioning
+
+    # CORS
+    BACKEND_CORS_ORIGINS: List[str] = [
+        "http://localhost:3000",  # React default
+        "http://localhost:5173",  # Vite default
+        "http://localhost:5174",  # Vite alternative port
+    ]
+
+    # Database
+    DATABASE_URL: str = "sqlite:///./myfinapp.db"
+
+
+settings = Settings()
